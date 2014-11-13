1417db27a19a7e01fc4d4386ad71210948b71157
Doug <drmsys@yahoo.com>
2014-11-12 20:15:57 -0800

    Updates for Chinese RTC board
    Mostly just set the time correctly

--- ChronoDot_20141027/ChronoDot_20141027.ino
+++ ChronoDot_20141027/ChronoDot_20141027.ino
@@ -1,11 +1,14 @@
 #include <Wire.h>
 #include <EEPROM.h>
 
-const char *version="ChronoDot_20141027 -> V3.0.3-20141109 ";
+const char *version="ChronoDot_20141027 -> V3.1.3-20141109 ";
+// A little tweeking to get to work with new clock module from ebay $1.59 from Seller: accecity2008 
+// Works with both now, china module has memory also.
+//
 const long msec_repeat=9000;
 const int num_regs = 19;
-// const int DS3231_addr = 0x68; // DS3231 I2C address ChronoDot
-const int DS3231_addr = 0x57; // DS3231 I2C address China Board
+const int DS3231_addr = 0x68; // DS3231 I2C address ChronoDot
+// const int DS3231_addr = 0x57; // DS3231 I2C address China Board
 unsigned long last_msec = 9999999; // initialize to weird value to assure quick first read
 unsigned long last_sec=0;
 
@@ -196,13 +199,13 @@ void set_time() { // This is how I bootstrap the time on the DS3231, you must ha
 //  Set Time
   Wire.write((byte) 0x00); // start at register 0
   Wire.write((byte) 0x00); // Seconds
-  Wire.write((byte) 0x24); // Minutes
-  Wire.write((byte) 0x07); // Hour register
+  Wire.write((byte) 0x05); // Minutes
+  Wire.write((byte) 0x20); // Hour register
 //  Set Date
-  Wire.write((byte) 0x03); // start at register 3
-  Wire.write((byte) 0x01); // Day of Week
-  Wire.write((byte) 0x28); // Day of Month
-  Wire.write((byte) 0x10); // Month
+//  Wire.write((byte) 0x03); // start at register 3
+  Wire.write((byte) 0x03); // Day of Week
+  Wire.write((byte) 0x12); // Day of Month
+  Wire.write((byte) 0x11); // Month
   Wire.write((byte) 0x14); // Year
   Wire.endTransmission();
 }
