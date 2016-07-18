fbad21c0aea19440f76bef7ba46ac5079113c525
Doug <drmsys@yahoo.com>
2016-07-18 10:01:50 -0700

    9 DoF integration

--- M0_Feather_LSM9DS0/M0_Feather_LSM9DS0.ino
+++ M0_Feather_LSM9DS0/M0_Feather_LSM9DS0.ino
@@ -1,65 +1,19 @@
+#include <drmLib.h>
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_LSM9DS0.h>
-
-/* This driver uses the Adafruit unified sensor library (Adafruit_Sensor),
-   which provides a common 'type' for sensor data and some helper functions.
-   
-   To use this driver you will also need to download the Adafruit_Sensor
-   library and include it in your libraries folder.
-
-   You should also assign a unique ID to this sensor for use with
-   the Adafruit Sensor API so that you can identify this particular
-   sensor in any data logs, etc.  To assign a unique ID, simply
-   provide an appropriate value in the constructor below (12345
-   is used by default in this example).
-   
-   Connections (For default I2C)
-   ===========
-   Connect SCL to analog 5
-   Connect SDA to analog 4
-   Connect VDD to 5V DC
-   Connect GROUND to common ground
-
-   History
-   =======
-   2014/JULY/25  - First version (KTOWN)
+/*  Works with M0 Feather BLE drm 20160708
+ *  This uses the Adafruit unified sensor library (Adafruit_Sensor),
 */
-   
+#define LED 13
+const char *version="M0_Feather_LSM9DS0 -> V0.1-20160709 ";
+
 /* Assign a unique base ID for this sensor */   
 Adafruit_LSM9DS0 lsm = Adafruit_LSM9DS0(1000);  // Use I2C, ID #1000
 
-
-/* Or, use Hardware SPI:
-  SCK -> SPI CLK
-  SDA -> SPI MOSI
-  G_SDO + XM_SDO -> tied together to SPI MISO
-  then select any two pins for the two CS lines:
-*/
-
-#define LSM9DS0_XM_CS 10
-#define LSM9DS0_GYRO_CS 9
-//Adafruit_LSM9DS0 lsm = Adafruit_LSM9DS0(LSM9DS0_XM_CS, LSM9DS0_GYRO_CS, 1000);
-
-/* Or, use Software SPI:
-  G_SDO + XM_SDO -> tied together to the MISO pin!
-  then select any pins for the SPI lines, and the two CS pins above
-*/
-
-#define LSM9DS0_SCLK 13
-#define LSM9DS0_MISO 12
-#define LSM9DS0_MOSI 11
-
-//Adafruit_LSM9DS0 lsm = Adafruit_LSM9DS0(LSM9DS0_SCLK, LSM9DS0_MISO, LSM9DS0_MOSI, LSM9DS0_XM_CS, LSM9DS0_GYRO_CS, 1000);
-
-
-/**************************************************************************/
-/*
-    Displays some basic information on this sensor from the unified
-    sensor API sensor_t type (see Adafruit_Sensor for more information)
-*/
-/**************************************************************************/
+//    Displays some basic information on this sensor from the unified
+//    sensor API sensor_t type (see Adafruit_Sensor for more information)
 void displaySensorDetails(void)
 {
   sensor_t accel, mag, gyro, temp;
@@ -109,43 +63,27 @@ void displaySensorDetails(void)
   delay(500);
 }
 
-/**************************************************************************/
-/*
-    Configures the gain and integration time for the TSL2561
-*/
-/**************************************************************************/
+//    Configures the gain and integration time for the TSL2561
 void configureSensor(void)
 {
   // 1.) Set the accelerometer range
   lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_2G);
-  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_4G);
-  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_6G);
-  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_8G);
-  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_16G);
   
   // 2.) Set the magnetometer sensitivity
   lsm.setupMag(lsm.LSM9DS0_MAGGAIN_2GAUSS);
-  //lsm.setupMag(lsm.LSM9DS0_MAGGAIN_4GAUSS);
-  //lsm.setupMag(lsm.LSM9DS0_MAGGAIN_8GAUSS);
-  //lsm.setupMag(lsm.LSM9DS0_MAGGAIN_12GAUSS);
 
   // 3.) Setup the gyroscope
   lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_245DPS);
-  //lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_500DPS);
-  //lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_2000DPS);
 }
 
-/**************************************************************************/
-/*
-    Arduino setup function (automatically called at startup)
-*/
-/**************************************************************************/
+unsigned long last_mclk = 0;
+#define mclk_interval 2000
 void setup(void) 
-{
-  while (!Serial);  // wait for flora/leonardo
-  
+{  
+  pinMode(LED, OUTPUT);
+  delay(2000);
   Serial.begin(115200);
-  Serial.println(F("LSM9DS0 9DOF Sensor API-drm Test")); Serial.println("");
+  drmStartPrint(version);
   
   /* Initialise the sensor */
   if(!lsm.begin())
@@ -174,30 +112,61 @@ void setup(void)
 /**************************************************************************/
 void loop(void) 
 {  
-  /* Get a new sensor event */ 
-  sensors_event_t accel, mag, gyro, temp;
+  if((last_mclk + mclk_interval) > millis()) return;
+  last_mclk = millis();
+  int i;
+  unsigned long startmilli = millis();
+  for (i=0; i<3 ; i++)
+  {
+    /* Get a new sensor event */
+    digitalWrite(LED, HIGH);
+    sensors_event_t accel, mag, gyro, temp;
+    lsm.getEvent(&accel, &mag, &gyro, &temp); 
+    digitalWrite(LED, LOW);
+    
+    // print out accelleration data
+    Serial.print("Accel X: "); Serial.print(accel.acceleration.x); Serial.print(" ");
+    Serial.print("  \tY: "); Serial.print(accel.acceleration.y);       Serial.print(" ");
+    Serial.print("  \tZ: "); Serial.print(accel.acceleration.z);     Serial.print("  \tm/s^2");
+    Serial.print("  \tmag: "); Serial.println(sqrt(accel.acceleration.x*accel.acceleration.x + 
+                                                   accel.acceleration.y*accel.acceleration.y + 
+                                                   accel.acceleration.z*accel.acceleration.z));
 
-  lsm.getEvent(&accel, &mag, &gyro, &temp); 
-
-  // print out accelleration data
-  Serial.print("Accel X: "); Serial.print(accel.acceleration.x); Serial.print(" ");
-  Serial.print("  \tY: "); Serial.print(accel.acceleration.y);       Serial.print(" ");
-  Serial.print("  \tZ: "); Serial.print(accel.acceleration.z);     Serial.println("  \tm/s^2");
-
-  // print out magnetometer data
-  Serial.print("Magn. X: "); Serial.print(mag.magnetic.x); Serial.print(" ");
-  Serial.print("  \tY: "); Serial.print(mag.magnetic.y);       Serial.print(" ");
-  Serial.print("  \tZ: "); Serial.print(mag.magnetic.z);     Serial.println("  \tgauss");
+    // print out magnetometer data
+    Serial.print("Magn. X: "); Serial.print(mag.magnetic.x); Serial.print(" ");
+    Serial.print("  \tY: "); Serial.print(mag.magnetic.y);       Serial.print(" ");
+    Serial.print("  \tZ: "); Serial.print(mag.magnetic.z);     Serial.print("  \tgauss");
+    Serial.print("  \tmag: "); Serial.println(sqrt(mag.magnetic.x*mag.magnetic.x + 
+                                                   mag.magnetic.y*mag.magnetic.y + 
+                                                   mag.magnetic.z*mag.magnetic.z));
+    
+    // print out gyroscopic data
+    Serial.print("Gyro  X: "); Serial.print(gyro.gyro.x); Serial.print(" ");
+    Serial.print("  \tY: "); Serial.print(gyro.gyro.y);       Serial.print(" ");
+    Serial.print("  \tZ: "); Serial.print(gyro.gyro.z);     Serial.print("  \tdps");
+    Serial.print("  \tmag: "); Serial.println(sqrt(gyro.gyro.x*gyro.gyro.x + 
+                                                   gyro.gyro.y*gyro.gyro.y + 
+                                                   gyro.gyro.z*gyro.gyro.z));
   
-  // print out gyroscopic data
-  Serial.print("Gyro  X: "); Serial.print(gyro.gyro.x); Serial.print(" ");
-  Serial.print("  \tY: "); Serial.print(gyro.gyro.y);       Serial.print(" ");
-  Serial.print("  \tZ: "); Serial.print(gyro.gyro.z);     Serial.println("  \tdps");
+    // print out accelleration corrected data
+    //Serial.print("Accel X: "); Serial.print(accel.acceleration.x); Serial.print(" ");
+    //Serial.print("  \tY: "); Serial.print(accel.acceleration.y);       Serial.print(" ");
+    //Serial.print("  \tZ: "); Serial.print(accel.acceleration.z);     Serial.println("  \tm/s^2");
+  
+    // print out magnetometer corrected data
+    //Serial.print("Magn. X: "); Serial.print(mag.magnetic.x); Serial.print(" ");
+    //Serial.print("  \tY: "); Serial.print(mag.magnetic.y);       Serial.print(" ");
+    //Serial.print("  \tZ: "); Serial.print(mag.magnetic.z);     Serial.println("  \tgauss");
+    
+    // print out gyroscopic corrected data
+    //Serial.print("Gyro-cX: "); Serial.print(gyro.gyro.x+1.9); Serial.print(" ");
+    //Serial.print("  \tY: "); Serial.print(gyro.gyro.y+1);       Serial.print(" ");
+    //Serial.print("  \tZ: "); Serial.print(gyro.gyro.z-0.5);     Serial.println("  \tdps");
+    
+    // print out temperature data
+    Serial.print("Temp: "); Serial.print(temp.temperature); Serial.println(" *C");
+  }
 
-  // print out temperature data
-  Serial.print("Temp: "); Serial.print(temp.temperature); Serial.println(" *C");
-
-  Serial.println("**********************\n");
-
-  delay(3000);
+  Serial.print(millis() - startmilli);
+  Serial.println(" ms **********************\n");
 }
