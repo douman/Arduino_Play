8e4eb95b4ac43770ab84d3ec1ed0db1c896027d9
Doug <drmsys@yahoo.com>
2016-10-30 13:21:28 -0700

    Got it working again (at Home)
    I don't understand Git

--- WiFi/Fea_M0_Connect_WPA_AIO/Fea_M0_Connect_WPA_AIO.ino
+++ WiFi/Fea_M0_Connect_WPA_AIO/Fea_M0_Connect_WPA_AIO.ino
@@ -4,38 +4,28 @@
  * Connection of Feather M0 to AdaFruit IO for logging using MQTT
  * written (synthesized) by drm 20160926
  */
- const char *code_version="Fea_M0_Connect_WPA_AIO -> V1.0-20160926 ";
+const char *code_version="Fea_M0_Connect_WPA_AIO -> V1.0-20160926 ";
 #include <drmLib.h>
 
 #include "config.h"
 
-// AdafruitIO_WiFi aio(AIO_USERNAME, AIO_KEY, WIFI_SSID, WIFI_PASS);
-AdafruitIO_WiFi aio(AIO_USERNAME, AIO_KEY, WIFI_SSID,"");
-
-
-#define LED           13
-#define BATT          A7
-#define BAT_AVG_CNT   4
-
-// The lines included below are authentication information and are not in GitHub
-#include "C:\drm\PrivCode\WiFi_auth.h"
-#include "C:\drm\PrivCode\Adafruit_IO_auth.h"
-
 //------ The items below are defined in the WiFi_auth.h include file
-// #define WIFI_SSID       "your_ssid"
-// #define WIFI_PASS       "your_pass"
-
+// WIFI_SSID       WIFI_PASS       
 //------ The items below are defined in the Adafruit_IO_auth.h include file
-// #define AIO_USERNAME    "your_username"
-// #define AIO_KEY         "your_key"
+// AIO_USERNAME    AIO_KEY
+
+AdafruitIO_WiFi aio(AIO_USERNAME, AIO_KEY, WIFI_SSID, WIFI_PASS);
+// Set up the AIO feeds
+AdafruitIO_Feed *volts = aio.feed("volts");
+AdafruitIO_Feed *secs = aio.feed("secs");
+AdafruitIO_Feed *sout_buf = aio.feed("sout_buf");
 
 int status = WL_IDLE_STATUS;     // the Wifi radio's status
 float batt_volts;
 int serial_wrt_free;
 long millis_start;
 unsigned long last_run_secs = 0;
-boolean serPrt = true;
-// boolean serPrt = false;
+boolean serPrt = false; // true;
 
 void setup() 
 {
@@ -49,16 +39,17 @@ void setup()
   pinMode(BATT, INPUT); // Battery level adc input
   
   Serial.begin(115200);
+  delay(1000);
   while (!Serial) 
   {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-    drmStartPrint(code_version);
+  drmStartPrint(code_version);
 
   // check for the presence of the shield:
   if (WiFi.status() == WL_NO_SHIELD) 
   {
-    Serial.println("WiFi shield not present");
+    Serial.println("WiFi shield not present-spinning");
     // don't continue:
     while (true);
   }
@@ -76,10 +67,10 @@ void setup()
     Serial.print("Attempting to connect to WPA SSID: ");
     Serial.println(ssid);
     // Connect to WPA/WPA2 network:
-    // if (pass == "")
+    if (pass == "")
       status = WiFi.begin(ssid);
-    // else
-      // status = WiFi.begin(ssid, pass);
+    else
+      status = WiFi.begin(ssid, pass);
 
     // wait 5 seconds for connection:
     delay(5000);
@@ -89,79 +80,111 @@ void setup()
   printCurrentNet();
   printWifiData();
 
+  Serial.print("Connecting to Adafruit IO");
+
+  // connect to io.adafruit.com
+  aio.connect();
+
+  // wait for a connection
+  while(aio.status() < AIO_CONNECTED) 
+  {
+    Serial.print(".");
+    delay(500);
+  }
 }
 
 void loop() 
 {
-  // check the network connection once every 5 seconds:
-  printCurrentNet();
-  printWifiData();
+  if(Serial.available())
+  {
+    char c = Serial.read();
+    if(c == 'S' || c == 's') 
+    { 
+      serPrt = !serPrt; 
+      Serial.print("# SER swap "); 
+      if(serPrt) Serial.println(serPrt); 
+    }
+  }
+  int delay_sec = 15;
+  if((millis()-millis_start)/1000 > 120) delay_sec = 300;
+  if(last_run_secs + delay_sec < (millis()-millis_start)/1000) 
+  {
+    aio.run();
+    last_run_secs = (millis()-millis_start)/1000;
+    printCurrentNet();
+    printWifiData();    
+    secs->save(last_run_secs);
+    volts->save(batt_volts);
+    sout_buf->save(serial_wrt_free);
+  }
+  // WiFi.maxLowPowerMode();
+  WiFi.lowPowerMode();
 }
 
 void printWifiData() 
 {
   // print your WiFi shield's IP address:
   IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
+  if(serPrt) Serial.print("IP Address: ");
+  if(serPrt) Serial.println(ip);
 
   // print your MAC address:
   byte mac[6];
   int i;
   WiFi.macAddress(mac);
-  Serial.print("MAC address: ");
+  if(serPrt) Serial.print("MAC address: ");
   for(i=5; i>=0; i--)
   {
-    Serial.print(mac[i], HEX);
-    if (i!=0) Serial.print(":");      
+    if(serPrt) Serial.print(mac[i], HEX);
+    if (i!=0) if(serPrt) Serial.print(":");      
   }
 }
 
 void printCurrentNet() 
 {
   // print the SSID of the network you're attached to:
-  Serial.println();
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
+  if(serPrt) Serial.println();
+  if(serPrt) Serial.print("SSID: ");
+  if(serPrt) Serial.println(WiFi.SSID());
 
   // Print run time
-  Serial.print("Running: ");
-  Serial.println((millis() - millis_start)/1000);
+  if(serPrt) Serial.print("Running: ");
+  if(serPrt) Serial.println((millis() - millis_start)/1000);
   
   // Get voltage, store in global and print
-  Serial.print("Volts: ");
-  Serial.println(batt_volts = read_batt());
+  if(serPrt) Serial.print("Volts: ");
+  if(serPrt) Serial.println(batt_volts = read_batt());
 
   // Get serial buffer free, store in global and print
-  Serial.print("WriteFree: ");
-  Serial.println(serial_wrt_free = Serial.availableForWrite());
+  if(serPrt) Serial.print("WriteFree: ");
+  if(serPrt) Serial.println(serial_wrt_free = Serial.availableForWrite());
 
   // print the MAC address of the router you're attached to:
   byte bssid[6];
   WiFi.BSSID(bssid);
-  Serial.print("BSSID: ");
-  Serial.print(bssid[5], HEX);
-  Serial.print(":");
-  Serial.print(bssid[4], HEX);
-  Serial.print(":");
-  Serial.print(bssid[3], HEX);
-  Serial.print(":");
-  Serial.print(bssid[2], HEX);
-  Serial.print(":");
-  Serial.print(bssid[1], HEX);
-  Serial.print(":");
-  Serial.println(bssid[0], HEX);
+  if(serPrt) Serial.print("BSSID: ");
+  if(serPrt) Serial.print(bssid[5], HEX);
+  if(serPrt) Serial.print(":");
+  if(serPrt) Serial.print(bssid[4], HEX);
+  if(serPrt) Serial.print(":");
+  if(serPrt) Serial.print(bssid[3], HEX);
+  if(serPrt) Serial.print(":");
+  if(serPrt) Serial.print(bssid[2], HEX);
+  if(serPrt) Serial.print(":");
+  if(serPrt) Serial.print(bssid[1], HEX);
+  if(serPrt) Serial.print(":");
+  if(serPrt) Serial.println(bssid[0], HEX);
 
   // print the received signal strength:
   long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.println(rssi);
+  if(serPrt) Serial.print("signal strength (RSSI):");
+  if(serPrt) Serial.println(rssi);
 
   // print the encryption type:
   byte encryption = WiFi.encryptionType();
-  Serial.print("Encryption Type:");
-  Serial.println(encryption, HEX);
-  Serial.println();
+  if(serPrt) Serial.print("Encryption Type:");
+  if(serPrt) Serial.println(encryption, HEX);
+  if(serPrt) Serial.println();
 }
 
 float read_batt()
--- WiFi/Fea_M0_Connect_WPA_AIO/config.h
+++ WiFi/Fea_M0_Connect_WPA_AIO/config.h
@@ -1,5 +1,9 @@
 // AIO and WiFi configuration stuff
 
+// The lines included below are authentication information and are not in GitHub
+#include "C:\drm\PrivCode\WiFi_auth.h"
+#include "C:\drm\PrivCode\Adafruit_IO_auth.h"
+
 #include <SPI.h>
 #include "AdafruitIO_WiFi.h"
 // replaced by AdafuitIO_WiFi.h -- #include <WiFi101.h>
@@ -15,15 +19,5 @@
 #define BATT          A7
 #define BAT_AVG_CNT   4
 
-// The lines included below are authentication information and are not in GitHub
-#include "C:\drm\PrivCode\WiFi_auth.h"
-#include "C:\drm\PrivCode\Adafruit_IO_auth.h"
-
-//------ The items below are defined in the WiFi_auth.h include file
-// #define WIFI_SSID       "your_ssid"
-// #define WIFI_PASS       "your_pass"
-
-//------ The items below are defined in the Adafruit_IO_auth.h include file
-// #define AIO_USERNAME    "your_username"
-// #define AIO_KEY         "your_key"
-
+void printCurrentNet();
+void printWifiData();
